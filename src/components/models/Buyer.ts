5ad237b5c223afcc8f9401663de0fa542e079d41
Aidar Zaynutdinov <aidar-dev@mail.ru>
2026-04-04 12:11:36 +0300

    И так я создал три класса: покупатель, корзина и каталог Создал типы для покупателя Написал класс покупателя Описал ридмишку

---
+++ src/components/models/Buyer.ts
@@ -0,0 +1,46 @@
+import type { IBuyer, TErrorsBayer } from "../../types";
+
+const emptyBuyerData = {
+    payment: null,
+    address: '',
+    phone: '',
+    email: ''
+}
+
+class Buyer {
+    private buyer: IBuyer;
+
+    constructor(buyerParam: IBuyer) {
+        this.buyer = buyerParam
+    }
+
+    get buyerData(): IBuyer {
+        return this.buyer;
+    }
+
+    set buyerData(Buyer: IBuyer) {
+        this.buyer = Buyer;
+    }
+
+    clearBuyerData(): void {
+        this.buyer = {...emptyBuyerData};
+    }
+
+    validateForm(): TErrorsBayer {
+        const errors: TErrorsBayer = {...emptyBuyerData};
+        if (this.buyer.address === '') {
+            errors.address = 'Поле \'Адрес\' не может быть пустым'
+        }
+        if (this.buyer.phone === '') {
+            errors.phone = 'Поле \'Телефон\' не может быть пустым'
+        }
+        if (this.buyer.email === '') {
+            errors.email = 'Поле \'Эл. почта\' не может быть пустым'
+        }
+        if (this.buyer.payment === null) {
+            errors.payment = 'Поле \'Способ оплаты\' не может быть пустым'
+        }
+
+        return errors;
+    }
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,28 @@
 export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
 
+type TPayment = 'card' | 'cash' | null;
+
 export interface IApi {
     get<T extends object>(uri: string): Promise<T>;
     post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
 }
+
+export interface IProduct {
+    id: string;
+    title: string;
+    description: string;
+    image: string;
+    category: string;
+    price: number | null;
+}
+
+export interface IBuyer {
+    payment: TPayment;
+    email: string;
+    phone: string;
+    address: string;
+}
+
+export type TErrorsBayer = Omit<IBuyer, "payment"> & {
+    payment: string | null;
+};
